06de60b924e8d1f2746217dc962c3c7fa5f52270
patrick brisbin <pbrisbin@gmail.com>
2013-09-11 18:00:36 -0400

    Make prompt functions more idiomatic zsh
    * Now loadable via the prompt system
    * Used (( )) for numeric comparisons

--- pure.zsh
+++ pure.zsh
@@ -3,7 +3,6 @@
 # https://github.com/sindresorhus/pure
 # MIT License
 
-
 # For my own and others sanity
 # git:
 # %b => current branch
@@ -17,63 +16,68 @@
 # %m => shortname host
 # %(?..) => prompt conditional - %(condition.true.false)
 
+# fastest possible way to check if repo is dirty
+prompt_pure_git_dirty() {
+        # check if we're in a git repo
+        command git rev-parse --is-inside-work-tree &>/dev/null || return
+        # check if it's dirty
+        command git diff --quiet --ignore-submodules HEAD &>/dev/null
 
-() {
-	autoload -Uz add-zsh-hook
-	autoload -Uz vcs_info
-
-	add-zsh-hook precmd pure_precmd
-	add-zsh-hook preexec pure_preexec
-
-	zstyle ':vcs_info:*' enable git
-	zstyle ':vcs_info:git*' formats ' %b'
-	zstyle ':vcs_info:git*' actionformats ' %b|%a'
-
-	# enable prompt substitution
-	setopt prompt_subst
-
-	# show username@host if logged in through SSH
-	if [[ $SSH_CLIENT != '' || $SSH_TTY != '' ]]; then
-		local username='%n@%m '
-	fi
-
-	# fastest possible way to check if repo is dirty
-	pure_git_dirty() {
-		# check if we're in a git repo
-		command git rev-parse --is-inside-work-tree &>/dev/null || return
-		# check if it's dirty
-		command git diff --quiet --ignore-submodules HEAD &>/dev/null; [ $? -eq 1 ] && echo '*'
-	}
-
-	# displays the exec time of the last command if set threshold was exceeded
-	pure_cmd_exec_time() {
-		local stop=`date +%s`
-		local start=${cmd_timestamp:-$stop}
-		integer elapsed=$stop-$start
-		[[ $elapsed -gt ${PURE_CMD_MAX_EXEC_TIME:=5} ]] && echo ${elapsed}s
-	}
-
-	pure_preexec() {
-		cmd_timestamp=`date +%s`
-
-		# shows the current dir and executed command in the title when a process is active
-		print -Pn "\e]0;$PWD:t: $2\a"
-	}
-
-	pure_precmd() {
-		# shows the full path in the title
-		print -Pn '\e]0;%~\a'
-
-		# git info
-		vcs_info
-
-		# add `%*` to display the time
-		print -P '\n%F{blue}%~%F{8}$vcs_info_msg_0_`pure_git_dirty` $username%f %F{yellow}`pure_cmd_exec_time`%f'
-
-		# reset value since `preexec` isn't always triggered
-		unset cmd_timestamp
-	}
-
-	# prompt turns red if the previous command didn't exit with 0
-	PROMPT='%(?.%F{magenta}.%F{red})❯%f '
+        (( $? == 1 )) && echo '*'
 }
+
+# displays the exec time of the last command if set threshold was exceeded
+prompt_pure_cmd_exec_time() {
+        local stop=`date +%s`
+        local start=${cmd_timestamp:-$stop}
+        integer elapsed=$stop-$start
+        (( $elapsed > ${PURE_CMD_MAX_EXEC_TIME:=5} )) && echo ${elapsed}s
+}
+
+prompt_pure_preexec() {
+        cmd_timestamp=`date +%s`
+
+        # shows the current dir and executed command in the title when a process is active
+        print -Pn "\e]0;$PWD:t: $2\a"
+}
+
+prompt_pure_precmd() {
+        # shows the full path in the title
+        print -Pn '\e]0;%~\a'
+
+        # git info
+        vcs_info
+
+        # add `%*` to display the time
+        print -P '\n%F{blue}%~%F{8}$vcs_info_msg_0_`prompt_pure_git_dirty` $username%f %F{yellow}`prompt_pure_cmd_exec_time`%f'
+
+        # reset value since `preexec` isn't always triggered
+        unset cmd_timestamp
+}
+
+
+prompt_pure_setup() {
+        set -g username
+
+        prompt_opts=( cr subst percent )
+
+        autoload -Uz add-zsh-hook
+        autoload -Uz vcs_info
+
+        add-zsh-hook precmd prompt_pure_precmd
+        add-zsh-hook preexec prompt_pure_preexec
+
+        zstyle ':vcs_info:*' enable git
+        zstyle ':vcs_info:git*' formats ' %b'
+        zstyle ':vcs_info:git*' actionformats ' %b|%a'
+
+        # show username@host if logged in through SSH
+        if [[ $SSH_CLIENT != '' || $SSH_TTY != '' ]]; then
+                username='%n@%m '
+        fi
+
+        # prompt turns red if the previous command didn't exit with 0
+        PROMPT='%(?.%F{magenta}.%F{red})❯%f '
+}
+
+prompt_pure_setup "$@"
