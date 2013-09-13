941782ea93088fe51cc73b71dfc573b061266c91
Hemanth HM <hemanth.hm@gmail.com>
2013-09-13 19:22:46 +0530

    Fixed no upstream issue

--- pure.zsh
+++ pure.zsh
@@ -75,4 +75,13 @@ prompt_pure_setup() {
 	PROMPT='%(?.%F{magenta}.%F{red})❯%f '
 }
 
+# Auto notify git pull!
+function prompt_pull() {
+    git rev-parse --is-inside-work-tree &>/dev/null &&
+    [[ $(git rev-list HEAD...origin/master --count 2>/dev/null) > 0 ]] &&
+    echo "You have stuff to pull..."
+}
+
+add-zsh-hook precmd prompt_pull
+
 prompt_pure_setup "$@"
