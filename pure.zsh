4a44483f5be5ad1cf4b1687f101c5faa9ee414c4
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-13 12:38:59 +0300

    use ecma-48 ansi sequences to restore cursor
    during precmd async command the escape sequences didn't handle the case where you had already begun typing, and they mangled the line. This change uses 'ESC [ s' and 'ESC [ u' to restore the cursor to the position you were typing in. Also it uses 'ESC [ 1 G' to start counting at the beginning of the line before printing the check result.

--- pure.zsh
+++ pure.zsh
@@ -66,7 +66,7 @@ prompt_pure_precmd() {
 		command git fetch &>/dev/null &&
 		(( $(command git rev-list --count HEAD...@'{u}' 2>/dev/null) > 0 )) &&
 		# some crazy ansi magic to inject the symbol into the previous line
-		print -Pn "\e[A\e[`prompt_pure_string_length $prompt_pure_preprompt`C%F{yellow}⇣%f\n\e[2C"
+		print -Pn "\e[s\e[A\e[1G\e[`prompt_pure_string_length $prompt_pure_preprompt`C%F{yellow}⇣%f\e[u"
 	} &!
 
 	# reset value since `preexec` isn't always triggered
