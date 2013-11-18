2a5b9b95a1a7dbc1c4ce83571204bdbe447a7c8c
Sindre Sorhus <sindresorhus@gmail.com>
2013-11-18 09:15:36 +0100

    Prevent `%` from showing on unfinished lines

--- pure.zsh
+++ pure.zsh
@@ -77,6 +77,10 @@ prompt_pure_precmd() {
 
 
 prompt_pure_setup() {
+	# prevent percentage showing up
+	# if output doesn't end with a newline
+	export PROMPT_EOL_MARK=''
+
 	prompt_opts=(cr subst percent)
 
 	autoload -Uz add-zsh-hook
