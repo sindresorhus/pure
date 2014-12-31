f38e463b3c744e1e1598ac713c575cdf705419d6
Kanstantsin Kamkou <kkamkou@gmail.com>
2014-12-31 13:39:05 +0700

    Close #100 PR: PURE_PROMPT_SYMBOL added.

--- pure.zsh
+++ pure.zsh
@@ -121,7 +121,7 @@ prompt_pure_setup() {
 	[[ $UID -eq 0 ]] && prompt_pure_username='%F{white}%n%F{242}@%m '
 
 	# prompt turns red if the previous command didn't exit with 0
-	PROMPT='%(?.%F{magenta}.%F{red})❯%f '
+	PROMPT="%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f "
 }
 
 prompt_pure_setup "$@"
