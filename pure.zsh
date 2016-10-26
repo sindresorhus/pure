57dabe4260b6067128f85ed3fb0d26912dd4569a
Valerio Battaglia <vabatta@gmail.com>
2016-10-26 18:03:14 +0200

    Added LSCOLORS to the theme

--- pure.zsh
+++ pure.zsh
@@ -324,6 +324,12 @@ prompt_pure_async_callback() {
 }
 
 prompt_pure_setup() {
+  # Define colors for BSD ls.
+  export LSCOLORS='exfxcxdxbxGxDxabagacad'
+
+  # Define colors for the completion system.
+  export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+
 	# prevent percentage showing up
 	# if output doesn't end with a newline
 	export PROMPT_EOL_MARK=''
