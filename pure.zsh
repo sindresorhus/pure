586d47c0f9e7a6367c2515929f71ed547811c822
Morten Noddeland <morten@noddeland.no>
2017-05-16 15:08:31 +0200

    Replace '${psvar[12]}' with '%12v' as requested in #325
    Nothing actually changes in this commit. '${psvar[12]}' and '%12v' are
    just two different ways to access the same value. The change is done for
    consistency.

--- pure.zsh
+++ pure.zsh
@@ -461,7 +461,7 @@ prompt_pure_setup() {
 	[[ $UID -eq 0 ]] && prompt_pure_username='%F{white}%n%f%F{242}@%m%f'
 
 	# if a virtualenv is activated, display it in grey
-	PROMPT='%(12V.%F{242}${psvar[12]}%f .)'
+	PROMPT='%(12V.%F{242}%12v%f .)'
 
 	# prompt turns red if the previous command didn't exit with 0
 	PROMPT+='%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
