aac53f783a835bd1d3dc7cbb145dc4496ab72f16
Arunvel Sriram <arunvelsriram@gmail.com>
2018-02-24 20:39:32 +0530

    Add variable for overriding default prompt symbol color

--- pure.zsh
+++ pure.zsh
@@ -465,7 +465,7 @@ prompt_pure_setup() {
 	PROMPT='%(12V.%F{242}%12v%f .)'
 
 	# prompt turns red if the previous command didn't exit with 0
-	PROMPT+='%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
+	PROMPT+='%(?.%F{${PURE_PROMPT_SYMBOL_COLOR:-magenta}}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
 }
 
 prompt_pure_setup "$@"
