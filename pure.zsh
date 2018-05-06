sindresorhus/pure

commit 17e23aecda563f6135b91d0d2eea82dadb0ca070

Try to detech SSH connection when SSH_CONNECTION is unset (#393)
This change allows us to detect SSH connections even when the
`$SSH_CONNECTION` environment variable is gone. This can happen when a
user changes to another user (`su`, `sudo`) or the environment is
otherwise reset.

By checking the output of `who am i` for a parenthesis at the end, we
assume that said parenthesis must contain either an IPv4 or IPv6
address, indicating that this is a remote session.

The reliablity of this assumption has not been analyzed to a great
degree, there could be false positives, or systems that do not format
the output in this way.

Fixes #382.

pure.zsh
@@ -134,7 +134,7 @@ prompt_pure_preprompt_render() {
 	fi
 
 	# Username and machine, if applicable.
-	[[ -n $prompt_pure_username ]] && preprompt_parts+=('$prompt_pure_username')
+	[[ -n $prompt_pure_state[username] ]] && preprompt_parts+=('${prompt_pure_state[username]}')
 	# Execution time.
 	[[ -n $prompt_pure_cmd_exec_time ]] && preprompt_parts+=('%F{yellow}${prompt_pure_cmd_exec_time}%f')
 
@@ -445,6 +445,8 @@ prompt_pure_async_callback() {
 }
 
 prompt_pure_setup() {
+	setopt localoptions noshwordsplit
+
 	# Prevent percentage showing up if output doesn't end with a newline.
 	export PROMPT_EOL_MARK=''
 
@@ -470,11 +472,25 @@ prompt_pure_setup() {
 	add-zsh-hook precmd prompt_pure_precmd
 	add-zsh-hook preexec prompt_pure_preexec
 
+	local ssh_connection=$SSH_CONNECTION
+	local username
+	if [[ -z $ssh_connection ]] && (( $+commands[who] )); then
+		# When changing user on a remote system, the $SSH_CONNECTION
+		# environment variable can be lost, attempt detection via who.
+		if who am i | grep -q '(.*)$' &>/dev/null; then
+			ssh_connection=true
+		fi
+	fi
+
 	# show username@host if logged in through SSH
-	[[ "$SSH_CONNECTION" != '' ]] && prompt_pure_username='%F{242}%n@%m%f'
+	[[ -n $ssh_connection ]] && username='%F{242}%n@%m%f'
 
 	# show username@host if root, with username in white
-	[[ $UID -eq 0 ]] && prompt_pure_username='%F{white}%n%f%F{242}@%m%f'
+	[[ $UID -eq 0 ]] && username='%F{white}%n%f%F{242}@%m%f'
+
+	typeset -gA prompt_pure_state=(
+		username "$username"
+	)
 
 	# if a virtualenv is activated, display it in grey
 	PROMPT='%(12V.%F{242}%12v%f .)'

commit 75aaf3b13619e5341db6461b9ff08be63b63e8c9

Prevent hostname from showing up in local X sessions
This change prevents the hostname from showing up when a terminal is
opened inside a local X session (displayed as (:1)).

Fixes a regression introduced in #393.

pure.zsh
@@ -484,7 +484,8 @@ prompt_pure_state_setup() {
 	if [[ -z $ssh_connection ]] && (( $+commands[who] )); then
 		# When changing user on a remote system, the $SSH_CONNECTION
 		# environment variable can be lost, attempt detection via who.
-		if who am i | grep -q '(.*)$' &>/dev/null; then
+		# Ignore local X sessions displayed as (:1).
+		if who am i | grep -v '(:[0-9]*)$' | grep -q '(.*)$' &>/dev/null; then
 			ssh_connection=true
 		fi
 	fi
@@ -496,7 +497,9 @@ prompt_pure_state_setup() {
 	[[ $UID -eq 0 ]] && username='%F{white}%n%f%F{242}@%m%f'
 
 	typeset -gA prompt_pure_state
-	prompt_pure_state=(username "$username")
+	prompt_pure_state=(
+		username "$username"
+	)
 }
 
 prompt_pure_setup() {