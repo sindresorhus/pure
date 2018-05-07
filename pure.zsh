60f54c65d3cd8198e83cc652dbbd32545dcf793c
Mathias Fredriksson <mafredri@gmail.com>
2018-05-07 20:23:00 +0300

    Improve the debug prompt (PS4) (#396)
    * Improve the debug prompt (PS4)
    
    Add colors, to highlight the current function and de-empasize line numbers.
    
    Add an extra line number (`%I`) which represents the line number in the file
    where the code resides (by default, only the function line number is shown,
    making it hard(er) to find the relevant code).
    
    Repeat the + character to indicate the current execution depth, e.g. when a 
    function calls another, the depth is increased. This allows you to quickly see 
    which function invoked which.
    
    * Add filename to debug prompt
    
    Add filename (without path) to the debug prompt, when applicable and makes sure
    to avoid displaying duplicate information. Say, when a file is being parsed,
    both %N and %x contain the same value (file path). In this case, we only show
    one value.
    
    There is a slight cost to doing the extra processing when testing in
    iTerm (unscientific benchmark):
    
    	zsh -x  0.30s user 0.14s system 57% cpu 0.754 total
    
    vs
    
    	zsh -x  0.49s user 0.16s system 65% cpu 0.988 total
    
    I think this is within an acceptable range.

--- pure.zsh
+++ pure.zsh
@@ -532,6 +532,33 @@ prompt_pure_setup() {
 
 	# prompt turns red if the previous command didn't exit with 0
 	PROMPT+='%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
+
+	# Store prompt expansion symbols for in-place expansion via (%). For
+	# some reason it does not work without storing them in a variable first.
+	typeset -ga prompt_pure_debug_depth
+	prompt_pure_debug_depth=('%e' '%N' '%x')
+
+	# Compare is used to check if %N equals %x. When they differ, the main
+	# prompt is used to allow displaying both file name and function. When
+	# they match, we use the secondary prompt to avoid displaying duplicate
+	# information.
+	local -A ps4_parts
+	ps4_parts=(
+		depth 	  '%F{yellow}${(l:${(%)prompt_pure_debug_depth[1]}::+:)}%f'
+		compare   '${${(%)prompt_pure_debug_depth[2]}:#${(%)prompt_pure_debug_depth[3]}}'
+		main      '%F{blue}${${(%)prompt_pure_debug_depth[3]}:t}%f%F{242}:%I%f %F{242}@%f%F{blue}%N%f%F{242}:%i%f'
+		secondary '%F{blue}%N%f%F{242}:%i'
+		prompt 	  '%F{242}>%f '
+	)
+	# Combine the parts with conditional logic. First the `:+` operator is
+	# used to replace `compare` either with `main` or an ampty string. Then
+	# the `:-` operator is used so that if `compare` becomes an empty
+	# string, it is replaced with `secondary`.
+	local ps4_symbols='${${'${ps4_parts[compare]}':+"'${ps4_parts[main]}'"}:-"'${ps4_parts[secondary]}'"}'
+
+	# Improve the debug prompt (PS4), show depth by repeating the +-sign and
+	# add colors to highlight essential parts like file and function name.
+	PROMPT4="${ps4_parts[depth]} ${ps4_symbols}${ps4_parts[prompt]}"
 }
 
 prompt_pure_setup "$@"
