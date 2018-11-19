e407d9e7a22b0c881aad15377ec1431f06dc7e86
Yoni Weill <joweill@icloud.com>
2018-11-19 14:47:33 +0200

    Add support for conda environments.

--- pure.zsh
+++ pure.zsh
@@ -185,6 +185,8 @@ prompt_pure_precmd() {
 	if [[ -n $VIRTUAL_ENV ]] && [[ -z $VIRTUAL_ENV_DISABLE_PROMPT || $VIRTUAL_ENV_DISABLE_PROMPT = 12 ]]; then
 		psvar[12]="${VIRTUAL_ENV:t}"
 		export VIRTUAL_ENV_DISABLE_PROMPT=12
+	elif [[ -n $CONDA_DEFAULT_ENV ]]; then
+		psvar[12]="${CONDA_DEFAULT_ENV}"
 	fi
 
 	# Make sure VIM prompt is reset.
