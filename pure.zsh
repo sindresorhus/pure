a4a10e1f7dae76bee0fca952d7d20af519dc0270
Samantha McVey <samantham@posteo.net>
2019-02-26 00:40:02 +0100

    Allow setting pre-prompt by user ENV var
    This will allow you to add stuff before the start of the prompt and/or
    change the color of the path.

--- pure.zsh
+++ pure.zsh
@@ -113,9 +113,9 @@ prompt_pure_preprompt_render() {
 
 	# Initialize the preprompt array.
 	local -a preprompt_parts
-
+	local default_prompt_pre='%F{blue}'
 	# Set the path.
-	preprompt_parts+=('%F{blue}%~%f')
+	preprompt_parts+=(${PURE_PROMPT_PRE:-$default_prompt_pre}'%~%f')
 
 	# Add git branch and dirty status info.
 	typeset -gA prompt_pure_vcs_info
