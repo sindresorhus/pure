5b8dd49303b9b5e86cddfd7911fc8baed7455fd1
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-20 22:13:59 +0700

    Update the zsh-async version to reflect the code

--- async.zsh
+++ async.zsh
@@ -3,12 +3,12 @@
 #
 # zsh-async
 #
-# version: 1.7.1
+# version: 1.7.2
 # author: Mathias Fredriksson
 # url: https://github.com/mafredri/zsh-async
 #
 
-typeset -g ASYNC_VERSION=1.7.1
+typeset -g ASYNC_VERSION=1.7.2
 # Produce debug output from zsh-async when set to 1.
 typeset -g ASYNC_DEBUG=${ASYNC_DEBUG:-0}
 
