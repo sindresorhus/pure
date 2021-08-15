98a57d9b4ceb7ca966fc5c918f49752f386a23ee
Samuel Attard <samuel.r.attard@gmail.com>
2021-08-15 02:56:39 -0700

    Do not show hostname when running in GitHub Codespaces (#604)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- pure.zsh
+++ pure.zsh
@@ -692,8 +692,8 @@ prompt_pure_state_setup() {
 	# Show `username@host` if logged in through SSH.
 	[[ -n $ssh_connection ]] && username='%F{$prompt_pure_colors[user]}%n%f'"$hostname"
 
-	# Show `username@host` if inside a container.
-	prompt_pure_is_inside_container && username='%F{$prompt_pure_colors[user]}%n%f'"$hostname"
+	# Show `username@host` if inside a container and not in GitHub Codespaces.
+	[[ -z "${CODESPACES}" ]] && prompt_pure_is_inside_container && username='%F{$prompt_pure_colors[user]}%n%f'"$hostname"
 
 	# Show `username@host` if root, with username in default color.
 	[[ $UID -eq 0 ]] && username='%F{$prompt_pure_colors[user:root]}%n%f'"$hostname"
