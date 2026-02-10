fddec0fe5a8045907c9ba09df1a64c52fc8321f9
Sindre Sorhus <sindresorhus@gmail.com>
2026-02-10 14:17:39 +0700

    1.27.0

--- pure.zsh
+++ pure.zsh
@@ -694,7 +694,7 @@ prompt_pure_state_setup() {
 	[[ $UID -eq 0 ]] && username='%F{$prompt_pure_colors[user:root]}%n%f'"$hostname"
 
 	typeset -gA prompt_pure_state
-	prompt_pure_state[version]="1.26.0"
+	prompt_pure_state[version]="1.27.0"
 	prompt_pure_state+=(
 		username "$username"
 		prompt	 "${PURE_PROMPT_SYMBOL:-❯}"
